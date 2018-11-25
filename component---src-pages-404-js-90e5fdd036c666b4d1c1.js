0a143de1a816f6bdcb034cfec64f49847077f081
jcc2303 <cabrera.javier@gmail.com>
2018-11-25 16:42:27 +0100

    Updates

---
+++ component---src-pages-404-js-90e5fdd036c666b4d1c1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{132:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),s=n(137);t.default=function(){return u.a.createElement(s.a,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-90e5fdd036c666b4d1c1.js.map
