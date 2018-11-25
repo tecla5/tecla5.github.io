774038ab631969a65a70a1de1e25c44a8b339354
jcc2303 <cabrera.javier@gmail.com>
2018-11-25 17:17:26 +0100

    Updates

--- component---src-pages-page-2-js-baa6c28fb637288e7275.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{137:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(142),l=t(141);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-baa6c28fb637288e7275.js.map
